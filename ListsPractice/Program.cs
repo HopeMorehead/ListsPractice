c14d73b588ed3683e0e883f7edf6243d7310e389
HopeMorehead <splendidwonder@gmail.com>
2017-07-12 10:53:00 -0400

    list progress add and bool

--- ListsPractice/Program.cs
+++ ListsPractice/Program.cs
@@ -10,6 +10,82 @@ namespace ListsPractice
     {
         static void Main(string[] args)
         {
+
+
+            List<int> favNumbers = new List<int>();
+
+            //A list is index.
+            favNumbers.Add(7);
+            favNumbers.Add(25);
+            favNumbers.Add(30);
+
+            foreach (int number in favNumbers)
+            {
+               // Console.WriteLine(number);
+
+            }
+            Console.WriteLine(favNumbers.Count);
+            List<string> books = new List<string> {"Cat in the hat", "Toad nd frog", "Green eggs and ham"};
+            books.Insert(0, "Hoton hears a Who");
+            Console.WriteLine(books[0]);
+            //Inserts at the current index moving the previous element down one
+            books.Insert(0, "Horton hears a who");
+            Console.WriteLine(books[0]);
+            //Create a List and add 5 animals using the .Add()
+            //Print each animal in the list
+
+            //create the following list:
+            //A bool list {true, false, false,true,false}
+            //Loop through each value
+            //if the valuse is true print "Better bring an umbrella"
+            //if the value is false print "No rain today enjoy the sun"
+
+            List<string> animals = new List<string>();
+            animals.Add("pigs");
+            animals.Add("pigglets");
+            animals.Add("piggies");
+            animals.Add("mama pig");
+            animals.Add("daddy pig");
+
+            foreach( string pig in animals)
+            {
+
+                Console.WriteLine(pig);
+            }
+
+
+
+            List<bool> weather = new List<bool> { true,false,true,false};
+            foreach (bool temp in weather)
+            
+            {
+                if (temp==true)
+                {
+
+
+                    Console.WriteLine("better bring an umbrella");
+                }
+                else
+                {
+                    Console.WriteLine("No rain today enjoy the sun");
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
